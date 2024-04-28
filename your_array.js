322293c3a860f2b9c572bc6630cf7bae990d97ec
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-04-29 04:24:54 +0500

    Add files via upload

---
+++ your_array.js
@@ -0,0 +1,6 @@
+var transportation = ['Audi', 'Land Cruiser', 'Mercedes', 'Sports Bike'];
+var message = 'I would like to own a ';
+console.log("".concat(message, " ").concat(transportation[0]));
+console.log("".concat(message, " ").concat(transportation[1]));
+console.log("".concat(message, " ").concat(transportation[2]));
+console.log("".concat(message, " ").concat(transportation[3]));
---
+++ your_array.ts
@@ -0,0 +1,7 @@
+let transportation :string[] = ['Audi', 'Land Cruiser', 'Mercedes', 'Sports Bike'];
+let message :string = 'I would like to own a ';
+
+console.log(`${message} ${transportation[0]}`);
+console.log(`${message} ${transportation[1]}`);
+console.log(`${message} ${transportation[2]}`);
+console.log(`${message} ${transportation[3]}`);
